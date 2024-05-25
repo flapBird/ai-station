124600f0be82aa7df7a8407f0bb2744fa7d405b2
leeswal <leeswal123@gmail.com>
2024-05-25 14:53:45 +0800

    add shipfast

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 export function Footer() {
-    // const footerNavs = [
-    //     {
-    //         href: 'javascript:void()',
-    //         name: 'About'
-    //     },
-    //     {
-    //         href: 'javascript:void()',
-    //         name: 'Team'
-    //     },
-    //     {
-    //         href: 'javascript:void()',
-    //         name: 'Suuport'
-    //     }
-    // ]
+    const footerNavs = [
+        {
+            href: 'https://shipfa.st/?via=lees',
+            name: 'ShipFast'
+        },
+        // {
+        //     href: 'javascript:void()',
+        //     name: 'Team'
+        // },
+        // {
+        //     href: 'javascript:void()',
+        //     name: 'Suuport'
+        // }
+    ]
 
     return (
         <footer className="text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto md:px-8">
@@ -24,17 +24,18 @@ export function Footer() {
                     MagicBox AI Tools Directory, Discover the top AI tools of 2024 with the MagicBox AI Directory!
                 </p>
             </div>
-            {/*<ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">*/}
-            {/*    {*/}
-            {/*        footerNavs.map((item, idx) => (*/}
-            {/*            <li className=" hover:text-gray-800">*/}
-            {/*                <a key={idx} href={item.href}>*/}
-            {/*                    { item.name }*/}
-            {/*                </a>*/}
-            {/*            </li>*/}
-            {/*        ))*/}
-            {/*    }*/}
-            {/*</ul>*/}
+            <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
+                {
+                    footerNavs.map((item, idx) => (
+                        <li className=" hover:text-gray-800">
+                            <div className="flex items-center">
+                                <img src="/shipfast.png" className="w-6 sm:mx-auto" />
+                                <a className="flex items-center" key={idx} href={item.href}>{ item.name }</a>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
             <style jsx>{`
                 .svg-icon path,
                 .svg-icon polygon,
